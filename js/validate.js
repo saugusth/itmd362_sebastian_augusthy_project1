189eb5fded074d5447eabf1a8eeb9456f85c7026
saugusth <saugusth@hawk.iit.edu>
2020-02-24 19:46:57 -0600

    fixing js

--- js/validate.js
+++ js/validate.js
@@ -9,23 +9,21 @@ function validateForm1() {
     alert("You are now logged in")
     return true;
   }
-
-
 }
 
-function validateForm2 {
+function validateForm2() {
   var c = document.forms["sign-up_form"]["username"].value;
   var d = document.forms["sign-up_form"]["psw"].value;
   if (c == "" || d == "") {
     alert("Fields cannot be empty");
     return false;
-  } else {
-    alert("You are now logged in")
-    return true;
   }
-
-  if (c.length < 8) {
+  else if (c.length < 8) {
     alert("username must be more than 8 characters");
     return false;
+  } 
+  else {
+    alert("You are now logged in")
+    return true;
   }  
 }
