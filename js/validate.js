84504e109501685383d253a86c9d0c5bb284ddd6
saugusth <saugusth@hawk.iit.edu>
2020-02-24 19:02:39 -0600

    fixed coding

--- js/validate.js
+++ js/validate.js
@@ -3,10 +3,20 @@ function validateForm() {
   var b = document.forms["login_form"]["psw"].value;
   var c = document.forms["sign-up_form"]["username"].value;
   var d = document.forms["sign-up_form"]["psw"].value;
-  if (a == "" || b == "" || c == "" || d == "") {
+  if (a == "" || b == "") {
     alert("Fields cannot be empty");
     return false;
   } else {
     alert("You are now logged in")
+    return true;
+  }
+
+  if (c == "" || d == ""){
+    alert("Fields cannot be empty");
+    return false;
+  }
+  else{
+    alert("You are now logged in")
+    return true;  	
   }
 }
